c58013860801396fb5fa61652932d9b7c7d9720c
theprogmatheus <theprog.matheus@gmail.com>
2022-06-05 17:57:26 -0300

    First commit

---
+++ src/main/java/com/github/theprogmatheus/bullchat/BullChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.theprogmatheus.bullchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/theprogmatheus/bullchat/BullChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.theprogmatheus.bullchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
